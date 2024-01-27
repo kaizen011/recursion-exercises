baeff5ccc58e2ef8d184dfc3884f8e3aef40f50b
kaizen011 <kaizen011@users.noreply.github.com>
2024-01-27 13:12:14 +0100

    3. Factorial

--- recursion.js
+++ recursion.js
@@ -1,5 +1,7 @@
 // Here I'll provide a solution for each exercise
 
+
+// 1st 
 function sumRange(x){
     if(x == 1){
         return x;
@@ -9,11 +11,24 @@ function sumRange(x){
     }
 }
 
+
+// 2nd 
 function power(base, exponent){
     if(exponent == 1){
         return base;
     }
     else{
         return base * power(base, exponent - 1);
+    }
+}
+
+
+// 3rd
+function factorial(x){
+    if(x == 1){
+        return x;
+    }
+    else{
+        return x * factorial(x - 1);
     }
 }

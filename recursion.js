8cab055451f9d73a378f3588bf87d1b96b6b2a3f
kaizen011 <kaizen011@users.noreply.github.com>
2024-01-27 16:46:55 +0100

    Check all values in array - recurcion

--- recursion.js
+++ recursion.js
@@ -32,3 +32,25 @@ function factorial(x){
         return x * factorial(x - 1);
     }
 }
+
+
+// 4th
+var allAreLessThanSeven = all([1,2,9], function(num){
+	return num < 7;
+});
+
+console.log(allAreLessThanSeven); // false
+
+function all(array, callback){
+	var copy = copy || array.slice(); // shallow copies array
+
+	if(copy.length === 0) return true;
+
+	if(callback(copy[0])){
+		copy.shift(); // remove first element from array
+		return all(copy, callback);
+	} else {
+		return false;
+	}
+}
+

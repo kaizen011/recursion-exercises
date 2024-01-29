52f5e5c6c0b8977f8ebe192c52f617efe15442f1
kaizen011 <kaizen011@users.noreply.github.com>
2024-01-29 22:47:01 +0100

    Output a single-linked list, recursive solution

--- recursion.js
+++ recursion.js
@@ -118,3 +118,29 @@ generateFibs(n);
 return fibArray;
 
 }
+
+let list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null
+        }
+      }
+    }
+  };
+  
+  function printList(list) {
+  
+    alert(list.value); // output the current item
+  
+    if (list.next) {
+      printList(list.next); // do the same for the rest of the list
+    }
+  
+  }
+  
+  printList(list);
